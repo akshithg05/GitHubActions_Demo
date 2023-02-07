a5ea51f4f3899fb330fc7069e3dc907e87cc2f28
akshithg05 <90089033+akshithg05@users.noreply.github.com>
2023-02-07 12:30:42 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -30,6 +30,7 @@ while True:
     choice = input("Enter choice(1/2/3/4): ")
     # check if choice is one of the four options
 
+
     if choice in ('1', '2', '3', '4'):
         try:
             num1 = float(input("Enter first number: "))
@@ -38,14 +39,18 @@ while True:
             print("Invalid input. Please enter a number.")
             continue
 
+
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
+
 
         elif choice == '2':
             print(num1, "-", num2, "=", subtract(num1, num2))
 
+
         elif choice == '3':
             print(num1, "*", num2, "=", multiply(num1, num2))
+
 
         elif choice == '4':
             print(num1, "/", num2, "=", divide(num1, num2))
