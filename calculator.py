14ac05116793c9bf46821b5b8fda66e5565f7a30
akshithg05 <90089033+akshithg05@users.noreply.github.com>
2023-02-07 13:41:46 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,14 +1,16 @@
-def Add(a,b):
-    return a+b
+def Add(a, b):
+    return a + b
 
 
 def SayHello():
     print("Hello")
 
-def Subtract(a,b):
-    return a-b
+
+def Subtract(a, b):
+    return a - b
+
 
 if __name__ == '__main__':
     SayHello()
-    Add(3,4)
-    Subtract(5,6)
+    Add(3, 4)
+    Subtract(5, 6)
